70245b856f8f5b73bb1f4baaf1239c2c3dd6d853
Clément COQUILLE <cc.lle@protonmail.com>
2024-07-27 14:39:59 +0200

    gnome tweaks & libs

--- install/app-neovim.sh
+++ install/app-neovim.sh
@@ -1,6 +1,9 @@
-sudo add-apt-repository -y ppa:neovim-ppa/stable
-sudo apt update
-sudo apt install -y neovim
+mkdir -p ~/.local/build/
+git clone https://github.com/neovim/neovim ~/.local/build
+cd ~/.local/build/neovim
+git checkout stable
+make CMAKE_BUILD_TYPE=RelWithDebInfo
+cd build && cpack -G DEB && sudo dpkg -i nvim-linux64.deb
 
 if [ ! -d "$HOME/.config/nvim" ]; then
 	git clone https://github.com/LazyVim/starter ~/.config/nvim
--- install/libraries.sh
+++ install/libraries.sh
@@ -3,4 +3,5 @@ sudo apt install -y \
 	libssl-dev libreadline-dev zlib1g-dev libyaml-dev libreadline-dev libncurses5-dev libffi-dev libgdbm-dev libjemalloc2 \
 	libvips imagemagick libmagickwand-dev mupdf mupdf-tools \
 	redis-tools sqlite3 libsqlite3-0 libmysqlclient-dev \
-  patch libreadline6-dev libgmp-dev libgdbm6 libdb-dev uuid-dev
+  patch libreadline6-dev libgmp-dev libgdbm6 libdb-dev uuid-dev \
+  ninja-build gettext cmake unzip curl
